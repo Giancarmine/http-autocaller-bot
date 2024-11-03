28d659e153b578f9ff9ac0198c0e7aafd74bfb3f
El_Giancar <giancarmine.cuofano@gmail.com>
2024-11-03 21:30:57 +0100

    add a first implementation of makeHttpGetRequest

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
 import { Bot, Context, webhookCallback } from 'grammy';
+import axios from 'axios';
+
+async function makeHttpGetRequest(url: string) {
+  try {
+    const response = await axios.get(url);
+    console.log(`HTTP GET request successful. Status: ${response.status}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error making HTTP GET request:', error.message);
+    throw error;
+  }
+}
 
 export interface Env {
 	BOT_INFO: string;
@@ -20,9 +32,19 @@ export default {
 			console.log(`@${ctx.me.username} started!`);
 		});
 
-		bot.command("help", async (ctx: Context) => {
-			await ctx.reply("NO-ONE can help you.");
+		bot.command('help', async (ctx: Context) => {
+			await ctx.reply('NO-ONE can help you.');
 		});
+
+		// Schedule the HTTP GET request every 15 minutes
+		const scheduleHttpCall = () => {
+			const url = 'https://inverso-backend.onrender.com/api/items?populate=*&pagination[pageSize]=5';
+			makeHttpGetRequest(url).catch((error) => {
+				console.error('Failed to make HTTP GET request:', error.message);
+			});
+		};
+
+		const intervalId = setInterval(scheduleHttpCall, 15 * 60 * 1000); // Every 15 minutes
 
 		return webhookCallback(bot, 'cloudflare-mod')(request);
 	},
